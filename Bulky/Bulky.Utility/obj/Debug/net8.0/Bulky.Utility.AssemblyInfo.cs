df9c022c281d080422b714aa1c963d18cbb6c222
Iriajen obehi <obehiiriajen@gmail.com>
2025-11-23 10:44:04 +0100

    Update assembly metadata and cache file
    Updated the `AssemblyInformationalVersionAttribute` in `Bulky.Utility.AssemblyInfo.cs` to reflect a new build or commit hash. The version string was changed from `1.0.0+4bd75d20d4a73abed3a7be970a60eb1739a5137d` to `1.0.0+57264501ceeba9678a9885be03e80781d1e47b42`.
    
    Additionally, updated the hash in `Bulky.Utility.AssemblyInfoInputs.cache` to reflect the new state of the assembly inputs. This change is part of the automated build process.

--- Bulky/Bulky.Utility/obj/Debug/net8.0/Bulky.Utility.AssemblyInfo.cs
+++ Bulky/Bulky.Utility/obj/Debug/net8.0/Bulky.Utility.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bulky.Utility")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bd75d20d4a73abed3a7be970a60eb1739a5137d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57264501ceeba9678a9885be03e80781d1e47b42")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bulky.Utility")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bulky.Utility")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
